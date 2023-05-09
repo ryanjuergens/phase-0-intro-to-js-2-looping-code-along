88185e9bb75fbfaab538f577e9efabf3db2b25a0
Ryan Juergens <rjuergens@hotmail.com>
2023-05-09 09:56:45 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let thankCards = []
+    for(let i = 0; i < name.length; i++){
+        thankCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankCards
+}     
+
+function countDown(n){
+    let countdown = n
+    while( countdown > -1 )
+{ console.log(countdown--)}
+}
